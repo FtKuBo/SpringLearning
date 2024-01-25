878b6065a5818c7fa3e59d3b82636989866240ef
FtKuBo <semmarmehdi558@gmail.com>
2024-01-25 15:54:02 -0500

    changement

--- src/main/java/com/mehdi/UserProfileApi/services/UserProfilesService.java
+++ src/main/java/com/mehdi/UserProfileApi/services/UserProfilesService.java
@@ -1,19 +1,33 @@
 package com.mehdi.UserProfileApi.services;
 
-
+import com.mehdi.UserProfileApi.infra.UserProfile;
 import com.mehdi.UserProfileApi.infra.UserProfileRepository;
+import org.apache.catalina.User;
 
 public class UserProfilesService {
     private UserProfileRepository UserProfileRepository;
 
     public UserProfilesService(){
+
         UserProfileRepository = new UserProfileRepository();
     }
     public UserProfileDto createProfile(CreateUserPorfileDto Dto){
-        return new UserProfileDto();
+        UserProfile userProfile = toUserProfile(Dto);
+        UserProfileRepository.save(userProfile);
+
+        return toDto(userProfile);
     }
 
-    private UserProfile toUserProfile(CreateUserProfileDto dto){
-        return new UserProfile();
+    private UserProfile toUserProfile(CreateUserPorfileDto dto){
+        return new UserProfile(dto.name,dto.email);
+    }
+
+    private UserProfileDto toDto(UserProfile userProfile){
+        UserProfileDto dto = new UserProfileDto();
+        dto.email = userProfile.getEmail();
+        dto.id = userProfile.getId();
+        dto.name = userProfile.getName();
+
+        return dto;
     }
 }
